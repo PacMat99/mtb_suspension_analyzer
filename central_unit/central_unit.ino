7070087f80f61efbf9bcc6514f5b83b6c93c0013
PacMat99 <pacchinmattia@gmail.com>
2023-03-21 14:14:13 +0100

    remove joystick code

--- central_unit/central_unit.ino
+++ central_unit/central_unit.ino
@@ -1,13 +1,10 @@
 #include "display.h"
-#include "joystick.h"
 
 void setup() {
   Serial.begin(115200);
   display_setup();
-  joystick_setup();
 }
 
 void loop() {
   display_loop();
-  joystick_loop();
 }
