e09cd73b87133d51c4b4bb58cf1a6594307ed4af
PacMat99 <pacchinmattia@gmail.com>
2023-04-12 10:21:07 +0200

    add secondary unit

--- secondary_unit/secondary_unit.ino
+++ secondary_unit/secondary_unit.ino
@@ -1,19 +1,13 @@
 #include <ESP8266WiFi.h>
-#include "display.h"
-#include "controller.h"
 #include "tof.h"
 
 void setup() {
   Serial.begin(115200);
-  display_setup();
-  controller_setup();
   tof_setup();
   Serial.println(WiFi.macAddress());
 }
 
 void loop() {
-  //display_loop();
-  //controller_loop();
   //tof_loop();
   delay(100);
 }
